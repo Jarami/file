eeefb0ed3f6343a8a840dfa9d7e64ad6318d5c3d
Jarami <vavilov86@yandex.ru>
2019-02-24 19:38:48 +0300

    scrollbar transition

--- js/lib/scrolling.js
+++ js/lib/scrolling.js
@@ -14,13 +14,14 @@ function Scrolling(elem, options, callback){
     height: "100%",
     width: "10px",
     backgroundColor: "rgba(100,100,100,0.5)",
-    display: "none"
+    opacity: 0,
+    transition: "opacity 0.8s"
   })
 
   var scrollbar = document.createElement("div");
   extend(scrollbar.style, {
     width: "10px",
-    height: elem.offsetHeight*elem.offsetHeight/elem.scrollHeight + "px",
+    height: 0 /*elem.offsetHeight*elem.offsetHeight/elem.scrollHeight + "px"*/,
     borderRadius: "5px",
     border: "1px solid grey",
     position: "relative",
@@ -37,17 +38,38 @@ function Scrolling(elem, options, callback){
     if (callback) callback();
   })
 
-  window.addEventListener("resize", function(e){
-    scrollbar.style.height = elem.offsetHeight*elem.offsetHeight/elem.scrollHeight + "px";
-  })
-  elem.addEventListener("resize", function(e){
-    console.log(e);
-  })
 
-  this.draw = function(){
-    scrollbarContainer.style.display = "block";
+  function reheight(){
     scrollbar.style.height = elem.offsetHeight*elem.offsetHeight/elem.scrollHeight + "px";
   }
+  function show(){
+    scrollbarContainer.style.opacity = 1;
+  }
+  function hide(){
+    scrollbarContainer.style.opacity = 0;
+  }
+  this.draw = function(){
+    if (elem.scrollHeight>elem.offsetHeight){
+      reheight();
+    } else {
+      hide();
+    }
+  }
+
+  window.addEventListener("resize", function(e){
+    if (elem.scrollHeight>elem.offsetHeight){
+      reheight();
+    }
+  })
+  elem.addEventListener("mouseover", function(){
+    if (elem.scrollHeight>elem.offsetHeight){
+      reheight();
+      show();
+    }
+  })
+  elem.addEventListener("mouseleave", function(){
+    hide();
+  })
 }
 Scrolling.init = function(elem, options, callback){
   return new Scrolling(elem, options, callback)
